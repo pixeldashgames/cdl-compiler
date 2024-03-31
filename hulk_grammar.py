871ca4531918db700c96421014859693601a06e3
Nuebe <ernesto.caballero@datys.cu>
2024-03-31 00:00:51 -0400

    ...♠

--- hulk_grammar.py
+++ hulk_grammar.py
@@ -1,20 +1,21 @@
 from utils.pycompiler import Grammar
 import hulk_ast as ast
+#from Parsing.parser import LR1Parser
 
 HG = Grammar()
 
 # No Terminales
 program = HG.NonTerminal('<program>', startSymbol=True)
-entity_list, def_type, def_func, expr = HG.NonTerminal('<entity-list> <def-class> <def-func> <expression>')
-expr_list, param_list, arg_list = HG.NonTerminal('<expr-list> <param-list> <arg-list>')
-abst_expr_list, empty_expr_list = HG.NonTerminal('<abst-expr-list> <empty-expr-list>')
-abst_param_list, empty_param_list = HG.NonTerminal('<abst-param-list> <empty-param-list>')
-abst_arg_list, empty_arg_list = HG.NonTerminal('<abst-arg-list> <empty-arg-list>')
-param, arith, term, factor, atom, boolean = HG.NonTerminals('<parameter> <arith> <term> <factor> <atom>')
+entity_list, def_type, def_func, expr = HG.NonTerminals('<entity-list> <def-class> <def-func> <expression>')
+expr_list, param_list, arg_list = HG.NonTerminals('<expr-list> <param-list> <arg-list>')
+abst_expr_list, empty_expr_list = HG.NonTerminals('<abst-expr-list> <empty-expr-list>')
+abst_param_list, empty_param_list = HG.NonTerminals('<abst-param-list> <empty-param-list>')
+abst_arg_list, empty_arg_list = HG.NonTerminals('<abst-arg-list> <empty-arg-list>')
+param, arith, term, factor, atom = HG.NonTerminals('<parameter> <arith> <term> <factor> <atom>')
 boolean, b_or, b_and, b_not, b_rel = HG.NonTerminals('<boolean> <ors> <ands> <nots> <relation>')
-cond, loop = HG.NonTerminal('<conditional> <loop>')
-func_call, meth_call, def_meth, def_attr = HG.NonTerminal('<func-call> <meth-call> <def_meth> <def-attr>')
-feature_list, abstract_feature_list, empty_feature_list= HG.NonTerminal('<feature-list> <abstract-feature-list> <empty-feature-list>')
+cond, loop = HG.NonTerminals('<conditional> <loop>')
+func_call, meth_call, def_meth, def_attr = HG.NonTerminals('<func-call> <meth-call> <def_meth> <def-attr>')
+feature_list, abstract_feature_list, empty_feature_list= HG.NonTerminals('<feature-list> <abstract-feature-list> <empty-feature-list>')
 asig_list, asig, des_asig= HG.NonTerminals('<asig-list> <asig> <destructive-asig>')
 
 # Terminales
@@ -167,3 +168,7 @@ arg_list %= expr, lambda h,s: [s[1]]
 arg_list %= expr + comma + arg_list, lambda h,s: [s[1]] + s[3]
 
 empty_arg_list %= HG.Epsilon, lambda h,s: []
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+#parser = LR1Parser(HG)
+#print("Terrible.♠")
